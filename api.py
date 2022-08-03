fe66ee51450a4eeae8548fbfdf96ad8d30aca747
Priya1208-dot <S10247708@connect.np.edu.sg>
2022-08-03 14:50:58 +0800

    file path added by priya

--- api.py
+++ api.py
@@ -5,3 +5,5 @@ api_key = 'F2UE00AWKLCI7DKV'
 url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
 response = requests.get(url)
 data = response.json()
+
+file_path = Path.cwd()/'summary_report.txt'
