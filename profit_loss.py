d189e032b1892c3c6428069629da4a452e8b1362
marcelle-S10241940 <S10241940@connect.np.edu.sg>
2022-08-03 15:43:26 +0800

    added to profit loss function (germaine)

--- profit_loss.py
+++ profit_loss.py
@@ -7,5 +7,12 @@ pl_csv = Path.cwd()/'csv_reports'/'Profit and Loss.csv'
 def profitloss_function(forex):
     if pl_csv.exists():
         with pl_csv.open(mode = 'r', encoding = 'UTF-8', errors = 'ignore') as csvfile:
+            reader = csv.reader(csvfile)
+            day = []
+            netprofit = []
+            next(reader)
+            for row in reader:
+                day.append(float(row[0]))
+                netprofit.append(float(row[4]) * forex)
     else:
         print('profitloss_function: pl_csv does not exist')
