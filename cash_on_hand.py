bac15ce272fcc907fa8cdeb4872c99138a587f31
s10242349 <s10242349@connect.np.edu.sg>
2022-08-03 15:27:25 +0800

    clara added to cash on hand function

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -7,6 +7,18 @@ coh_csv = Path.cwd()/'csv_reports'/'Cash on Hand.csv'
 def cashonhand_function(forex):
     if coh_csv.exists():
         with coh_csv.open(mode = 'r', encoding = 'UTF-8', errors = 'ignore') as csvfile:
-    
+            reader = csv.reader(csvfile)
+            day = []
+            cash = []
+            next(reader)
+            for row in reader:
+                day.append(float(row[0]))
+                cash.append(float(row[1]) * forex)
     else:
         print('cashonhand_function: File path does not exist')
+    count = 0
+    for amount in range(len(cash) - 1):
+
+    if count == 0:
+        if file_path.exists():
+            with file_path.open(mode = 'a', encoding = 'UTF-8', errors = 'ignore') as file:
