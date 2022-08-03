258d71d7cd32c2ef99e120dc443afd6e00779aed
Priya1208-dot <S10247708@connect.np.edu.sg>
2022-08-03 14:50:02 +0800

    data extracted by priya

---
+++ api.py
@@ -0,0 +1,7 @@
+import requests
+from pathlib import Path
+
+api_key = 'F2UE00AWKLCI7DKV'
+url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+response = requests.get(url)
+data = response.json()
